85817c8c083f9a05d659257db3175ac2bde65407
Md Soykot <md.soykot2910@gmail.com>
2023-05-22 10:38:25 +0600

    inited

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin',
+      database: 'products',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ProductsModule,
+  ],
+  controllers: [],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/products/products.controller.ts
@@ -0,0 +1,57 @@
+import { Controller, Get, Post, Body, Query, Res, Req } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Request, Response } from 'express';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  async create(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Body() createProductDto: CreateProductDto,
+  ) {
+    // return this.productsService.create(createProductDto);
+    let createData = await this.productsService.create(createProductDto);
+    return res.send(createData);
+  }
+
+  @Get()
+  async findAll(@Req() req: Request, @Res() res: Response) {
+    let findAll: any = await this.productsService.findAll();
+    return res.send(findAll);
+  }
+
+  @Get('/getOne')
+  async findOne(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Query('id') id: any,
+  ) {
+    let getOne = await this.productsService.findOne(id);
+    return res.send(getOne);
+  }
+
+  @Post('/update')
+  async update(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Query('id') id: string,
+    @Body() updateProductDto: CreateProductDto,
+  ) {
+    let updateData = await this.productsService.update(id, updateProductDto);
+    return res.send(updateData);
+  }
+
+  @Post('/delete')
+  async remove(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Query('id') id: string,
+  ) {
+    let deleteData = await this.productsService.remove(+id);
+    return res.send(deleteData);
+  }
+}
